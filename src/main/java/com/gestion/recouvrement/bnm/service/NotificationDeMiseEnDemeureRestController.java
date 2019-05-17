7e74609cd9d841530e3b85fcba147d632dfb8a7c
GDRBNM <GDRBNM@gmail.com>
2019-05-17 15:09:50 +0200

    project principale
    project principale

---
+++ src/main/java/com/gestion/recouvrement/bnm/service/NotificationDeMiseEnDemeureRestController.java
@@ -0,0 +1,69 @@
+package com.gestion.recouvrement.bnm.service;
+
+import com.gestion.recouvrement.bnm.dao.DocumentRepository;
+import com.gestion.recouvrement.bnm.dao.NotificationDeMiseEnDemeureRepository;
+import com.gestion.recouvrement.bnm.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+
+@CrossOrigin("*")
+@RestController
+public class NotificationDeMiseEnDemeureRestController {
+
+    @Autowired
+    NotificationDeMiseEnDemeureRepository notificationDeMiseEnDemeureRepository;
+
+    @Autowired
+    DocumentRepository documentRepository;
+    @PostMapping(value = "/SaveMED")
+    public NotificationDeMiseEnDemeure notificationDeMiseEnDemeure
+            (@RequestBody NotificationDeMiseEnDemeure MED,
+             @RequestParam("idClient") Long idClient,
+             @RequestParam("idHuissier") Long idHuissier,
+             @RequestParam("type") String type,
+            @RequestParam("file") MultipartFile file) throws Exception{
+        Huissier huissier = new Huissier();
+        Document document=new Document();
+        if (type.equals("Particulier")) {
+            Client a = new Particulier();
+            a.setId(idClient);
+            huissier.setId(idHuissier);
+            MED.setClient(a);
+            MED.setDateNotification(new Date());
+            MED.setHuissier(huissier);
+            NotificationDeMiseEnDemeure notificationDeMiseEnDemeure=notificationDeMiseEnDemeureRepository.save(MED);
+            //enregistrement du document
+            Files.write(Paths.get(System.getProperty("user.home")+"/IdeaProjects/GDR-pdf/pdf/"+file.getOriginalFilename()),file.getBytes());
+            //enregistrement de l'id de la notification de la mise en demeure
+            document.setNotificationDeMiseEnDemeure(notificationDeMiseEnDemeure);
+            document.setNomDocument(file.getOriginalFilename().toString());
+            documentRepository.save(document);
+            System.out.println(documentRepository.save(document));
+            return notificationDeMiseEnDemeureRepository.save(MED);
+        }
+return null;
+    }
+
+
+/*
+    @Autowired
+    NotificationDeMiseEnDemeureRepository notificationDeMiseEnDemeureRepository;
+    @PostMapping(value = "/SaveMED/{idClient}/{idHuissier}")
+    public NotificationDeMiseEnDemeure notificationDeMiseEnDemeure(@RequestBody NotificationDeMiseEnDemeure MED,
+                                                                   @PathVariable("idClient") Long idClient,@PathVariable("idHuissier") Long idHuissier){
+        Client a=new Particulier();
+        a.setId(idClient);
+        Huissier huissier=new Huissier();
+        huissier.setId(idHuissier);
+        MED.setClient(a);
+        MED.setHuissier(huissier);
+        return notificationDeMiseEnDemeureRepository.save(MED);
+    }
+*/
+
+}
